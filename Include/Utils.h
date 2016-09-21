9436bd0a3ea9e3a78c3e20bd6ad58f46cdc58992
Bojan Endrovski <bojan.endrovski@gmail.com>
2016-09-21 08:35:34 +0200

    More methods for working with strings

--- Include/Utils.h
+++ Include/Utils.h
@@ -25,6 +25,37 @@ bool SaveFile(const std::string& filename, const std::string& text);
 /// String hash using sdbm algorithm
 ullong StringHash(const std::string& str);
 
+/// Replace all occurrences of the search string with the replacement string.
+///
+/// @param subject The string being searched and replaced on, otherwise known as the haystack.
+/// @param search The value being searched for, otherwise known as the needle.
+/// @param replace The replacement value that replaces found search values.
+/// @return a new string with all occurances replaced.
+///
+std::string StringReplace(const std::string& subject,
+	const std::string& search,
+	const std::string& replace);
+
+/// Determine whether or not a string starts with the given prefix. Does
+/// not create an internal copy.
+///
+/// @param subject The string being searched in.
+/// @param prefix The string to search for.
+/// @return a boolean indicating if the prefix was found.
+///
+bool StringStartsWith(const std::string& subject,
+	const std::string& prefix);
+
+/// Determine whether or not a string ends with the given suffix. Does
+/// not create an internal copy.
+///
+/// @param subject The string being searched in.
+/// @param prefix The string to search for.
+/// @return a boolean indicating if the suffix was found.
+///
+bool StringEndsWith(const std::string& subject,
+	const std::string& suffix);
+
 /// Spherical coordinates defined with radius and two angles
 struct SphericalCoordinates
 {
