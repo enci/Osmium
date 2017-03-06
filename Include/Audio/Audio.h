a232a349ce26a32b9edb5c5bd5ae7ca2b391af50
Bojan Endrovski <bojan.endrovski@gmail.com>
2017-03-06 17:02:21 +0100

    Audio getting someplace (not far thou)

--- Include/Audio/Audio.h
+++ Include/Audio/Audio.h
@@ -1,14 +1,80 @@
 #pragma once
-#include <fmod.h>
+
 #include <Core/Engine.h>
+#include <Math/Vector3.h>
+#include <fmod_studio.hpp>
+#include <map>
 
 namespace Osm
 {
+	struct Vector3;
 
+	//	FMOD_VECTOR 
 class AudioManager : Component<CEngine>
 {
 public:
 	AudioManager(CEngine& owner);
+
+	virtual ~AudioManager();
+
+	void Update(float dt);
+
+	void LoadBank(const std::string& bankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags);
+
+	void LoadEvent(const std::string& eventName);
+
+	void Loadsound(	const std::string& soundName,
+					bool b3d = true,
+					bool bLooping = false,
+					bool bStream = false);
+
+	void UnLoadSound(const std::string& soundName);
+
+	void Set3dListenerAndOrientation(	const Vector3& vPos = Vector3(),
+										float fVolumedB = 0.0f);
+
+	void PlaySound(	const std::string& soundName,
+					const Vector3& vPos = Vector3(),
+					float volumedB = 0.0f);
+
+	void PlayEvent(const std::string& eventName);
+
+	void StopChannel(int channelId);
+
+	void StopEvent(	const std::string& eventName,
+					bool immediate = false);
+
+	void GetEventParameter(	const std::string& eventName,
+							const std::string& eventParameter,
+							float* parameter);
+
+	void SetEventParameter(	const std::string& eventName,
+							const std::string& parameterName,
+							float value);
+
+	void StopAllChannels();
+
+	void SetChannel3dPosition(int channelId, const Vector3& position);
+
+	void SetChannelvolume(int channelId, float volumedB);
+
+	bool IsPlaying(int channelId) const;
+
+	bool IsEventPlaying(const std::string& eventName) const;
+
+	float dbToVolume(float db);
+
+	float VolumeTodb(float volume);
+
+	FMOD_VECTOR VectorToFmod(const Vector3& vPosition);
+
+private:
+	FMOD::System*										_system			= nullptr;
+	FMOD::Studio::System*								_studioSystem	= nullptr;
+	std::map<std::string, FMOD::Sound*>					_sounds;
+	std::map<int, FMOD::Channel*>						_channels;
+	std::map<std::string, FMOD::Studio::EventInstance*> _events;
+	std::map<std::string, FMOD::Studio::Bank*>			_banks;
 };
 
 }
--- Source/Audio/Audio.cpp
+++ Source/Audio/Audio.cpp
@@ -3,6 +3,7 @@
 #include <Defines.h>
 
 using namespace Osm;
+using namespace std;
 
 int ErrorCheck(FMOD_RESULT result)
 {
@@ -15,7 +16,47 @@ int ErrorCheck(FMOD_RESULT result)
 }
 
 AudioManager::AudioManager(CEngine& owner) : Component(owner)
+{	
+	ErrorCheck(FMOD::Studio::System::create(&_studioSystem));
+	ErrorCheck(	_studioSystem->initialize(32,
+				FMOD_STUDIO_INIT_LIVEUPDATE,
+				FMOD_INIT_PROFILE_ENABLE,
+				nullptr));
+
+	ErrorCheck(_studioSystem->getLowLevelSystem(&_system));
+}
+
+AudioManager::~AudioManager()
 {
-	FMOD::Studio::System* mpStudioSystem;
-	ErrorCheck(FMOD::Studio::System::create(&mpStudioSystem));
+	ErrorCheck(_studioSystem->unloadAll());
+	ErrorCheck(_studioSystem->release());
+}
+
+void AudioManager::Update(float dt)
+{
+	// Remove inactive channels
+	for (auto it = _channels.begin(); it != _channels.end();)
+	{
+		bool isPlaying = false;
+		it->second->isPlaying(&isPlaying);
+		if (!isPlaying)
+			it = _channels.erase(it);
+		else
+			++it;
+	}
+	
+	ErrorCheck(_studioSystem->update());
+}
+
+void AudioManager::LoadBank(const std::string& bankName,
+							FMOD_STUDIO_LOAD_BANK_FLAGS flags)
+{
+	auto foundIt = _banks.find(bankName);
+	if (foundIt != _banks.end())
+		return;
+
+	FMOD::Studio::Bank* bank;
+	ErrorCheck(_studioSystem->loadBankFile(bankName.c_str(), flags, &bank));
+	if (bank) 		
+		_banks[bankName] = bank;
 }
